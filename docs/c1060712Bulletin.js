552fbda866ca3799cab7efcc5491659b76abd6ff
BwayCer <bwaycer@gmail.com>
2017-07-15 23:44:25 +0800

    增補 怎麼變動態磚了 未完待續

---
+++ docs/c1060712Bulletin.js
@@ -0,0 +1,60 @@
+/**
+ * 1060712 號公告
+ *
+ * @file
+ */
+
+"use strict";
+
+
+!function() {
+    var isPcMode = !~navigator.userAgent.indexOf( 'Mobile' );
+
+    !function() {
+        var bodyClassList = document.body.classList;
+
+        if ( isPcMode ) bodyClassList.add( 'onPc' );
+        else bodyClassList.add( 'onMobile' );
+    }();
+
+
+    document.querySelector( '.topLayer' ).onselectstart = function() { return false; };
+    document.querySelectorAll( '.toolBox_boxIcon' ).forEach( function ( helItem ) {
+        helItem.onselectstart = function() { return false; };
+    } );
+
+
+    if ( isPcMode ) {
+        document.querySelectorAll( '.toolBox > .toolBox_square, .toolBox > .toolBox_wide' )
+            .forEach( function ( helItem ) {
+                helItem.addEventListener( 'mouseenter', function () {
+                    this.classList.add( 'onShowTitle' );
+                } );
+
+                helItem.addEventListener( 'mouseleave', function () {
+                    this.classList.remove( 'onShowTitle' );
+                } );
+            } );
+    } else {
+        !function() {
+            var drawId = null;
+
+            document.getElementById( 'toolBox_squareMenu' )
+                .addEventListener( 'click', function () {
+                    if ( drawId ) cancelAnimationFrame( drawId );
+
+                    requestAnimationFrame( () => {
+                        drawId = null;
+
+                        var p, len;
+                        var brothers = this.parentNode.children;
+
+                        for ( p = 0, len = brothers.length; p < len ; p++ ) {
+                            brothers[ p ].classList.toggle( 'onShowTitle' );
+                        }
+                    } );
+                } );
+        }();
+    }
+}();
+
